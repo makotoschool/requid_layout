bc47cbe8653efbdde2714805e6b8464ae6bbc5ee
703koushi <703manabu@gmail.com>
2015-12-16 11:27:45 +0900

    上へ戻る作戦

--- requid+responsive+hotdog/js/main.js
+++ requid+responsive+hotdog/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 //ローディングメッセージを出す
+/*
 $('body').prepend('<div id="load"><i id="spin" class="fa fa-spinner fa-pulse"></i></div>');
 $('#load').css({
 			width:'100%',
@@ -16,9 +17,20 @@ $('#spin').css({
 			
 
 });
-
+*/
 //ホットドッグメニューを作る
-
+$('.nav').prepend('<p id="hotdog">三</p>');
+	$('#hotdog').css({
+					margin:0,
+					backgroundColor:'#3D3737',
+					color:'#fff',
+					height:'40px',
+					textAlign:'center',
+					lineHeight:'40px',
+					fontWeight:'bold',
+					fontSize:'2em',
+					cursor:'pointer'	
+	}).hide();//普段は隠しておく
 
 //ブラウザのウィンドウサイズを取得作戦
 //表示する箱をつくる
@@ -35,7 +47,7 @@ $('.container').after('<p id="valbox">現在<span id="val"></span>pxです</p>')
 	//ブラウザをリサイズした時の世界
 	$(window).resize(function(){
 		var wsize=$(window).width()+17;	
-		$('#val').text(wsize);
+		//$('#val').text(wsize);
 		if(wsize <= 400){
 			$('#hotdog').show();
 			$('.nav ul').hide();
@@ -47,7 +59,7 @@ $('.container').after('<p id="valbox">現在<span id="val"></span>pxです</p>')
 	});
 //リロードした時もホットドックメニュー作戦が実行されるように	
 var wsize=$(window).width()+17;	
-		$('#val').text(wsize);
+		//$('#val').text(wsize);
 		if(wsize <= 400){
 			$('#hotdog').show();
 			$('.nav ul').hide();
@@ -59,6 +71,42 @@ var wsize=$(window).width()+17;
 //ホットドッグメニューをクリックした世界
 $('#hotdog').click(function(){
 	$('.nav ul').slideToggle(1000);
-});		
+});	
+
+
+//上へ戻るボタン作戦
+//ロードきっかけでボタンを登場
+$('body').append('<p id="gotop">上へ戻る</p>');
+$('#gotop').css({
+			position:'fixed',
+			bottom:100,
+			right:20,
+			backgroundColor:'rgba(242,41,148,0.8)',
+			color:'#fff',
+			padding:20,
+			cursor:'pointer'
+			}).hide();
+
+//スクロール値を取得作戦
+	//ブラウザスクロールの世界
+	$(window).scroll(function(){
+		var sval=$('body,html').scrollTop();
+		$('#val').text(sval);
+		
+		if(sval>=800){
+			$('#gotop').fadeIn();
+		}else{
+			$('#gotop').fadeOut();
+		}
+	});
+
+//#gotopをclickした世界
+	$('#gotop').click(function(){
+		$('body,html').animate({scrollTop:0},1000,'swing');
+	
+	});
+
+	
+	
 
 });//End jquery
