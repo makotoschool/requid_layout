cf66d7ca36d53bc73e8ecb07e1060a60d2a18330
703koushi <703manabu@gmail.com>
2015-12-16 15:13:20 +0900

    bxslider up

---
+++ slide/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('#slide1').bxSlider({
+				'mode':'fade',
+				'controls':false,
+				'pager':false,
+				'auto':true,
+				'speed':5000,
+				
+	
+	
+				});
+
+
+
+});//End jquery
+
