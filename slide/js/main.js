b30d956a039c6e91e238d3a375578e99e961adaa
703koushi <703manabu@gmail.com>
2015-12-16 16:02:56 +0900

    title animateion added

--- slide/js/main.js
+++ slide/js/main.js
@@ -5,12 +5,25 @@ $(function(){
 				'pager':false,
 				'auto':true,
 				'speed':5000,
-				
-	
-	
+
 				});
+	$('#line').animate({width:'100%'},1000,'linear',function(){
+		$(this).animate({height:'200px'},1000,'linear');
+	});
+	$('#maintitle').animate({left:'70%'},2000,'swing',function(){
+		$(this).animate({fontSize:'800%',left:'20%',top:'48%'},2000,'linear');
+	
+	});
+	
+	setTimeout(function(){
+			$('#line').animate({width:'20%',top:'-20',	height:'800px'	},1000,'linear',function(){
+				$('#maintitle').css({color:'#000'});					
+								
+			})
 
-
+	
+	},6000);
+	
 
 });//End jquery
 
